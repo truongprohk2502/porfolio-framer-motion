1f7b8cbfcf6d93d2123e39169f3df65717898fa6
Nguyen Dinh Truong <truongnd1@runsystem.net>
2024-01-25 16:28:56 +0700

    Completed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import "./app.scss";
+import Contact from "./components/contact";
+// import Cursor from "./components/cursor/Cursor";
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Parallax from "./components/parallax";
+import Portfolio from "./components/portfolio";
+import Services from "./components/services";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      {/* <Cursor /> */}
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/hero/index.jsx
@@ -0,0 +1,79 @@
+import "./styles.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <h2>HARVEY TYLER</h2>
+          <h1>Web developer and UI designer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Writer Content Creator Influencer
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
